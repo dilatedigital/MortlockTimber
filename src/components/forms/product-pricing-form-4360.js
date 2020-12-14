7569dd410b35a5e7a006b75abe61e5adf3cce1ac
jurryabbas-dev <74167360+jurryabbas-dev@users.noreply.github.com>
2020-12-14 14:25:47 +0500

    Update product-pricing-form-4360.js

--- src/components/forms/product-pricing-form-4360.js
+++ src/components/forms/product-pricing-form-4360.js
@@ -100,6 +100,26 @@ class ProductPricingForm extends Component {
     window.dataLayer = window.dataLayer || []
     window.dataLayer.push(this.props.gtag)
   }
+  
+	submission_page() {
+		var submission_page_url = window.location.href;
+		return submission_page_url;
+	}
+	
+function getUrlParameter(sParam) {
+		var sPageURL = window.location.search.substring(1),
+			sURLVariables = sPageURL.split('&'),
+			sParameterName,
+			i;
+
+		for (i = 0; i < sURLVariables.length; i++) {
+			sParameterName = sURLVariables[i].split('=');
+
+			if (sParameterName[0] === sParam) {
+				return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+			}
+		}
+	}
 
   handleSubmit = async event => {
     event.preventDefault()
@@ -224,30 +244,30 @@ class ProductPricingForm extends Component {
 
   render() {
     const { submitActive, popupActive, popupFormActive } = this.state;
-	var getUrlParameter = function getUrlParameter(sParam) {
-		var sPageURL = window.location.search.substring(1),
-			sURLVariables = sPageURL.split('&'),
-			sParameterName,
-			i;
+	// var getUrlParameter = function getUrlParameter(sParam) {
+		// var sPageURL = window.location.search.substring(1),
+			// sURLVariables = sPageURL.split('&'),
+			// sParameterName,
+			// i;
 
-		for (i = 0; i < sURLVariables.length; i++) {
-			sParameterName = sURLVariables[i].split('=');
+		// for (i = 0; i < sURLVariables.length; i++) {
+			// sParameterName = sURLVariables[i].split('=');
 
-			if (sParameterName[0] === sParam) {
-				return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-			}
-		}
-	};
+			// if (sParameterName[0] === sParam) {
+				// return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+			// }
+		// }
+	// };
 	
 	var pre_submission = function pre_submission() {
 		var pre_submission_page = '';
 		return pre_submission_page;
 	};	
 	
-	var submission_page = function submission_page() {
-		var submission_page_url = window.location.href;
-		return submission_page_url;
-	};	
+	// var submission_page = function submission_page() {
+		// var submission_page_url = window.location.href;
+		// return submission_page_url;
+	// };	
 	
 	this.state.fields.external_referral_site = pre_submission();
 	this.state.fields.landing_page = pre_submission();
