bdfd13e5f7a84c21df8caef8226590796b62f9e0
jurryabbas-dev <74167360+jurryabbas-dev@users.noreply.github.com>
2020-12-14 15:01:24 +0500

    Update landing-page-form-5153.js

--- src/components/forms/landing-page-form-5153.js
+++ src/components/forms/landing-page-form-5153.js
@@ -231,7 +231,8 @@ class LandingPageForm extends Component {
 		var submission_page_url = window.location.href;
 		return submission_page_url;
 	};	
-	
+
+	if (typeof window !== `undefined`){	
 	this.state.fields.external_referral_site = pre_submission();
 	this.state.fields.landing_page = pre_submission();
 	this.state.fields.pre_submission_page = pre_submission();
@@ -242,6 +243,7 @@ class LandingPageForm extends Component {
 	this.state.fields.utm_term = getUrlParameter('utm_term');
 	this.state.fields.utm_content = getUrlParameter('utm_content');
 	this.state.fields.gclid = getUrlParameter('gclid');	
+	}
 	
     return (
       <form className={submitActive ? 'contact__form loading' : 'contact__form'} id="landing-page-contact-form" type="POST" onSubmit={ this.handleSubmit } noValidate>
