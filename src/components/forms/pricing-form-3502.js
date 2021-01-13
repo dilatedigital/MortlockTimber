2dafe008aab07ef9cc3bcbe9f4c4be84abbe7089
jurryabbas-dev <74167360+jurryabbas-dev@users.noreply.github.com>
2021-01-13 13:53:55 +0500

    Update pricing-form-3502.js

--- src/components/forms/pricing-form-3502.js
+++ src/components/forms/pricing-form-3502.js
@@ -27,9 +27,6 @@ class PricingForm extends Component {
           {id: 4, name: "Shou Sugi Ban", value: "shousugiban", isChecked: false}
         ],
         products: [],
-		external_referral_site: '',
-		landing_page: '',
-		pre_submission_page: '',
 		submission_page: '',
 		utm_source: '',
 		utm_medium: '',
@@ -61,11 +58,46 @@ class PricingForm extends Component {
       submitActive: false,
       mainFormMsg: '',
       mainFormState: null,
-      popupActive: false
+      popupActive: false,
+	pre_submission_page: "",
+      external_referral_site: "",
+      landing_page: "",
     }
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleInputCheck = this.handleInputCheck.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+	
+  getLeadSource() {
+    const queryString = window.location.search
+    const urlParams = new URLSearchParams(queryString)
+    if (urlParams.has("utm_source")) {
+      this.setState({ leadInfoSource: urlParams.get("utm_source") })
+    } else {
+      this.setState({ leadInfoSource: "Organic" })
+    }
+  }
+  handleReferrer() {
+    setTimeout(() => {
+      if ((sessionStorage.getItem("referrer")).includes("mortlock.com.au")){
+      this.setState({
+        external_referral_site: "None",
+      })
+      }else{
+      this.setState({
+        external_referral_site: sessionStorage.getItem("referrer"),
+      })	      
+      }
+      this.setState({ landing_page: sessionStorage.getItem("landing") })
+      this.setState({
+        pre_submission_page: sessionStorage.getItem("referrer"),
+      })
+      console.log(sessionStorage.getItem("landing"))
+    }, 300)
+  }
+  componentDidMount() {
+    this.getLeadSource()
+    this.handleReferrer()
   }
 
   handleInputCheck(event) {
@@ -165,16 +197,22 @@ class PricingForm extends Component {
       bodyFormData.append('leadsource', this.state.fields.leadsource)
       bodyFormData.append('pageURL', this.state.fields.pageURL)
       bodyFormData.append('interest', this.state.fields.interest)
-      bodyFormData.append('external_referral_site', this.state.fields.external_referral_site)
-      bodyFormData.append('landing_page', this.state.fields.landing_page)
-      bodyFormData.append('pre_submission_page', this.state.fields.pre_submission_page)
-      bodyFormData.append('submission_page', this.state.fields.submission_page)
-      bodyFormData.append('utm_source', this.state.fields.utm_source)
-      bodyFormData.append('utm_medium', this.state.fields.utm_medium)
-      bodyFormData.append('utm_campaign', this.state.fields.utm_campaign)
-      bodyFormData.append('utm_term', this.state.fields.utm_term)
-      bodyFormData.append('utm_content', this.state.fields.utm_content)
-      bodyFormData.append('gclid', this.state.fields.gclid)
+      bodyFormData.append(
+        "external_referral_site",
+        this.state.external_referral_site
+      )
+      bodyFormData.append("landing_page", this.state.landing_page)
+      bodyFormData.append(
+        "pre_submission_page",
+        this.state.pre_submission_page
+      )
+      bodyFormData.append("submission_page", this.state.fields.submission_page)
+      bodyFormData.append("utm_source", this.state.fields.utm_source)
+      bodyFormData.append("utm_medium", this.state.fields.utm_medium)
+      bodyFormData.append("utm_campaign", this.state.fields.utm_campaign)
+      bodyFormData.append("utm_term", this.state.fields.utm_term)
+      bodyFormData.append("utm_content", this.state.fields.utm_content)
+      bodyFormData.append("gclid", this.state.fields.gclid)
 
       axios.post(formLink, bodyFormData, Helpers.config).then((res) => {
         if(res.data.status === 'mail_sent') {
@@ -201,9 +239,6 @@ class PricingForm extends Component {
                   {id: 4, name: "Shou Sugi Ban", value: "shousugiban", isChecked: false}
                 ],
                 products: this.state.fields.products,
-				external_referral_site: '',
-				landing_page: '',
-				pre_submission_page: '',
 				submission_page: '',
 				utm_source: '',
 				utm_medium: '',
@@ -235,44 +270,39 @@ class PricingForm extends Component {
   }
 
   render() {
-    const { submitActive, popupActive } = this.state;
-	var getUrlParameter = function getUrlParameter(sParam) {
-		var sPageURL = window.location.search.substring(1),
-			sURLVariables = sPageURL.split('&'),
-			sParameterName,
-			i;
-
-		for (i = 0; i < sURLVariables.length; i++) {
-			sParameterName = sURLVariables[i].split('=');
-
-			if (sParameterName[0] === sParam) {
-				return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-			}
-		}
-	};
-	
-	var pre_submission = function pre_submission() {
-		var pre_submission_page = '';
-		return pre_submission_page;
-	};	
-	
-	var submission_page = function submission_page() {
-		var submission_page_url = window.location.href;
-		return submission_page_url;
-	};	
-
-	if (typeof window !== `undefined`){	
-	this.state.fields.external_referral_site = pre_submission();
-	this.state.fields.landing_page = pre_submission();
-	this.state.fields.pre_submission_page = pre_submission();
-	this.state.fields.submission_page = submission_page();
-	this.state.fields.utm_source = getUrlParameter('utm_source');
-	this.state.fields.utm_medium = getUrlParameter('utm_medium');
-	this.state.fields.utm_campaign = getUrlParameter('utm_campaign');
-	this.state.fields.utm_term = getUrlParameter('utm_term');
-	this.state.fields.utm_content = getUrlParameter('utm_content');
-	this.state.fields.gclid = getUrlParameter('gclid');	
-	}
+    const { submitActive, popupActive } = this.state
+    var getUrlParameter = function getUrlParameter(sParam) {
+      var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split("&"),
+        sParameterName,
+        i
+      for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split("=")
+        if (sParameterName[0] === sParam) {
+          return sParameterName[1] === undefined
+            ? true
+            : decodeURIComponent(sParameterName[1])
+        }
+      }
+    }
+    var pre_submission = function pre_submission() {
+      var pre_submission_page = ""
+      return pre_submission_page
+    }
+    var submission_page = function submission_page() {
+      var submission_page_url = window.location.href
+      return submission_page_url
+    }
+    if (typeof window !== `undefined`) {
+      this.state.fields.pre_submission_page = pre_submission()
+      this.state.fields.submission_page = submission_page()
+      this.state.fields.utm_source = getUrlParameter("utm_source")
+      this.state.fields.utm_medium = getUrlParameter("utm_medium")
+      this.state.fields.utm_campaign = getUrlParameter("utm_campaign")
+      this.state.fields.utm_term = getUrlParameter("utm_term")
+      this.state.fields.utm_content = getUrlParameter("utm_content")
+      this.state.fields.gclid = getUrlParameter("gclid")
+    }
 	
     if(popupActive) {
       return (
@@ -380,27 +410,63 @@ class PricingForm extends Component {
               }
             </ul>
           </div>
-		<div style={{ display: `none` }}>
-	<input type="hidden" name="external_referral_site" value={ pre_submission() || ''} />
-
-	<input type="hidden" name="landing_page" value={ pre_submission() || ''} />
-
-	<input type="hidden" name="pre_submission_page" value={ pre_submission() || ''} />
-
-	<input type="hidden" name="submission_page" value={ typeof window !== `undefined` ?	window.location.href : ''} />
-
-	<input type="hidden" name="utm_source" value={ typeof window !== `undefined` ?	getUrlParameter('utm_source') : ''} />
-
-	<input type="hidden" name="utm_medium" value={ typeof window !== `undefined` ?	getUrlParameter('utm_medium') : ''} />
-
-	<input type="hidden" name="utm_campaign" value={ typeof window !== `undefined` ?	getUrlParameter('utm_campaign') : ''} />
-	 
-	<input type="hidden" name="utm_term" value={ typeof window !== `undefined` ?	getUrlParameter('utm_term') : ''} />
-	 
-	<input type="hidden" name="utm_content" value={ typeof window !== `undefined` ?	getUrlParameter('utm_content') : ''} />
-	 
-	<input type="hidden" name="gclid" value={ typeof window !== `undefined` ?	getUrlParameter('gclid') : ''} />
-</div>
+          <div style={{ display: `none` }}>
+            <input
+              type="hidden"
+              name="submission_page"
+              value={typeof window !== `undefined` ? window.location.href : ""}
+            />
+            <input
+              type="hidden"
+              name="utm_source"
+              value={
+                typeof window !== `undefined`
+                  ? getUrlParameter("utm_source")
+                  : ""
+              }
+            />
+            <input
+              type="hidden"
+              name="utm_medium"
+              value={
+                typeof window !== `undefined`
+                  ? getUrlParameter("utm_medium")
+                  : ""
+              }
+            />
+            <input
+              type="hidden"
+              name="utm_campaign"
+              value={
+                typeof window !== `undefined`
+                  ? getUrlParameter("utm_campaign")
+                  : ""
+              }
+            />
+            <input
+              type="hidden"
+              name="utm_term"
+              value={
+                typeof window !== `undefined` ? getUrlParameter("utm_term") : ""
+              }
+            />
+            <input
+              type="hidden"
+              name="utm_content"
+              value={
+                typeof window !== `undefined`
+                  ? getUrlParameter("utm_content")
+                  : ""
+              }
+            />
+            <input
+              type="hidden"
+              name="gclid"
+              value={
+                typeof window !== `undefined` ? getUrlParameter("gclid") : ""
+              }
+            />
+          </div>
           <div className="btn_wrap">
             <button className="button" type="submit"><span className="text">Submit</span><Loader /></button>
             {this.state.mainFormMsg && <span className={`form-msg ${this.state.mainFormState}`}>{ this.state.mainFormMsg }</span>}
