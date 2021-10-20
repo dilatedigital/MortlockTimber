cb46ef267c118d547927934d016754ad9347a3fc
root <root@gatsby.dev.cloud.dilatedigital.com.au>
2021-10-20 06:16:47 +0000

    Added Redirection

--- gatsby-node.js
+++ gatsby-node.js
@@ -20,6 +20,7 @@ exports.createPages = async ({ graphql, actions }) => {
 createRedirect({ fromPath: '/vacoa-timber-cladding/', toPath: '/vacoa-cladding/', isPermanent: true, redirectInBrowser: true })
 createRedirect({ fromPath: '/news/do-i-have-to-maintain-shou-sugi-ban-charred-timber/', toPath: '/news/how-to-maintain-shou-sugi-ban-cladding-for-maximum-durability/', isPermanent: true, redirectInBrowser: true })
 createRedirect({ fromPath: '/pricing-new/', toPath: '/product-pricing-guide/', isPermanent: true, redirectInBrowser: true })
+createRedirect({ fromPath: '/timber-ceilings/proplank-timber-battens/', toPath: '/timber-battens/', isPermanent: true, redirectInBrowser: true })
   // The “graphql” function allows us to run arbitrary
   // queries against the local Gatsby GraphQL schema. Think of
   // it like the site has a built-in database constructed
